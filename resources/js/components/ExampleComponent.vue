72915757868b60473b8e5e3c256b98e90a691494
neosolution <neosolution1998@gmail.com>
2022-08-06 02:13:35 +0300

    Реализована возможность удаления, переноса пункта и вычеркивания

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -10,13 +10,22 @@
                 </thead>
                 <tbody>
                     <tr v-for="item in items">
-                        <td v-if="!item.urgent">{{ item.name }}
-                            <a>
-                                <button type="button" class="btn btn-outline-success"
-                                    style="--bs-btn-padding-y: .20rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
-                                    move to urgent
-                                </button>
-                            </a>
+                        <td v-if="!item.urgent">
+
+                            <input type="checkbox" @change="updateCheck(item.id, item.completed)"
+                                v-model="item.completed">
+                            <span :class="[item.completed ? 'completed' : 'itemText']"> {{ item.name }}</span>
+
+                            <button type="button" @click="updateItem(item.id, item.urgent = true)" class="btn btn-link"
+                                style="--bs-btn-padding-y: .20rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                move to urgent
+                            </button>
+
+                            <button type="button" onclick="location.reload(); return false;"
+                                @click="deleteItem(item.id)" class="btn btn-link"
+                                style="--bs-btn-padding-y: .20rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                remove
+                            </button>
                         </td>
                     </tr>
                     <tr>
@@ -36,7 +45,21 @@
                 </thead>
                 <tbody>
                     <tr v-for="item in items">
-                        <td v-if="item.urgent">{{ item.name }}</td>
+                        <td v-if="item.urgent"><input type="checkbox" @change="updateCheck(item.id, item.completed)"
+                                v-model="item.completed">
+                            <span :class="[item.completed ? 'completed' : 'itemText']"> {{ item.name }}</span>
+
+                            <button type="button" @click="updateItem(item.id, item.urgent = false)" class="btn btn-link"
+                                style="--bs-btn-padding-y: .20rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                move to todos
+                            </button>
+
+                            <button type="button" onclick="location.reload(); return false;"
+                                @click="deleteItem(item.id)" class="btn btn-link"
+                                style="--bs-btn-padding-y: .20rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+                                remove
+                            </button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -50,7 +73,7 @@ export default {
     data() {
         return {
             items: {
-                name: ""
+                completed: true
             }
         }
     },
@@ -59,6 +82,57 @@ export default {
             .then(response => (this.items = response.data));
         console.log('response')
     },
-    
+    methods: {
+        updateCheck(id, completed) {
+            console.log(id);
+            const article = { completed: completed };
+            axios.put('/api/item/' + id, article)
+                .then(response => {
+                    if (response.status == 200) {
+                        console.log('response');
+
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+        },
+        updateItem(id, urgent) {
+            const article = { urgent: urgent };
+            axios.put('/api/item/' + id, article, {
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+                .then(response => {
+                    if (response.status == 200) {
+                        console.log(article);
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+        },
+        deleteItem(id) {
+            axios.delete('/api/item/' + id)
+                .then(response => {
+                    if (response.status == 200) {
+                        this.$emit('itemdeleted');
+                    }
+                })
+                .catch(error => {
+                    console.log(id);
+                })
+        }
+    }
 }
 </script>
+
+<style>
+.completed {
+    text-decoration: line-through;
+    color: #999999
+}
+</style>
